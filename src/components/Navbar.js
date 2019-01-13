1de4859bceec50bbdc06aaf7cc57dfa470305046
Ed Brannin <edbrannin@gmail.com>
2019-01-12 22:47:39 -0500

    Remove header link to gatsby-netlify-cms-boilerplate on GitHub

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,18 +39,7 @@ const Navbar = () => (
           Contact Us
         </a>
       </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
+      <div className="navbar-end"></div>
     </div>
   </nav>
 )
